69b18da020802d8a63970bf9da43d63e9395be52
neojarvis <techadmin@iamneo.ai>
2025-06-09 18:35:27 +0000

    Manual-save-commit-6/10/2025--12:05:24-AM

---
+++ springapp/src/main/java/com/examly/springapp/model/Flight.java
@@ -0,0 +1,15 @@
+package com.examly.springapp.model;
+
+public class Flight {
+    private long flightId;
+    private String flightNumber;
+    private String airline;
+    private String departureLocation;
+    private String arrivalLocation;
+    private String departureTime;
+    private String arrivalTime;
+    private double price;
+    private int totalSeats;
+
+    
+}
