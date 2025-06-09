c4b5004e38ad051f43cdfdb8f59acba3ed82818b
neojarvis <techadmin@iamneo.ai>
2025-06-09 19:06:25 +0000

    Manual-save-commit-6/10/2025--12:36:22-AM

--- springapp/src/main/java/com/examly/springapp/controller/TestController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TestController.java
@@ -1,14 +1,33 @@
 package com.examly.springapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.examly.springapp.model.Flight;
+import com.examly.springapp.service.FlightService;
+
 @RestController
 public class TestController {
+    FlightService flightService;
+
+    @Autowired
+    public void setFlightService(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
     @GetMapping("/api/test/welcome")
     public String testWelcomeApi (){
         return "Welcome to the Flight Booking Application";
     }
+
+    @GetMapping("api/test/flights")
+    public List<Flight> showFlights(){
+        return flightService.geFlights();
+    }
+    
 }
