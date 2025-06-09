d0a68174c93bd546ede526a45fde2279885b41d5
neojarvis <techadmin@iamneo.ai>
2025-06-09 18:16:38 +0000

    Manual-save-commit-6/9/2025--11:46:37-PM

--- springapp/src/main/java/com/examly/springapp/controller/TestController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.examly.springapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping
+    @GetMapping("/api/test/welcome")
+    public ResponseEntity<String> welcome(){
+        String s = "Welcome to the Flight Booking Application";
+        return new ResponseEntity<>(s,HttpStatus.CREATED);
+    }
 }
