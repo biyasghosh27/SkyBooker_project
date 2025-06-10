7af6751f35e93398ec550b3113a44fa529b5a31e
neojarvis <techadmin@iamneo.ai>
2025-06-10 21:11:34 +0000

    Manual-save-commit-6/11/2025--2:41:32-AM

--- springapp/src/main/java/com/examly/springapp/controller/FlightController.java
+++ springapp/src/main/java/com/examly/springapp/controller/FlightController.java
@@ -1,14 +1,17 @@
 package com.examly.springapp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.DeleteExchange;
 
@@ -35,13 +38,24 @@ public class FlightController {
     }
 
     @PostMapping("/api/flights")
+    public ResponseEntity<Flight> addFlight(@RequestBody Flight flight){
+        flight = flightService.createFlight(flight);
+        return new ResponseEntity<>(flight,HttpStatus.CREATED);
+    }
 
     @PutMapping("/api/flights/{flightId}")
+    public ResponseEntity<Flight> modifyFlight(@PathVariable int flightId, @RequestBody Flight flight){
+        Optional<Flight> optionalFlight = flightService.updatedFlight(flightId,flight);
+        if(optionalFlight.isPresent()){
+            return new ResponseEntity<>(optionalFlight.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
-    @GetMapping("/api/flights")
+    //@GetMapping("/api/flights")
 
-    @GetMapping("/api/flights/{flightId}")
+    //@GetMapping("/api/flights/{flightId}")
 
-    @DeleteMapping("/api/flights/{flightId}")
+    //@DeleteMapping("/api/flights/{flightId}")
     
 }
--- springapp/src/main/java/com/examly/springapp/service/FlightService.java
+++ springapp/src/main/java/com/examly/springapp/service/FlightService.java
@@ -1,10 +1,12 @@
 package com.examly.springapp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.examly.springapp.model.Flight;
 
 public interface FlightService {
     public List<Flight> getFlights();
-
+    public Flight createFlight(Flight flight);
+    public Optional<Flight> updateFlight(int flightId, Flight flightDeatils);
 }
