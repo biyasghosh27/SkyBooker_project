fed2d88c82e2c87c02af02a8677d3e823f2c338a
neojarvis <techadmin@iamneo.ai>
2025-06-15 05:26:10 +0000

    Submit-project-save-commit-6/15/2025--10:56:04-AM

--- springapp/src/main/java/com/examly/springapp/controller/BookingController.java
+++ springapp/src/main/java/com/examly/springapp/controller/BookingController.java
@@ -1,10 +1,18 @@
 package com.examly.springapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.examly.springapp.model.Booking;
 import com.examly.springapp.service.BookingService;
 import com.examly.springapp.service.BookingServiceImpl;
 
@@ -19,6 +27,36 @@ public class BookingController {
     }
 
     @PostMapping("/api/bookings")
-    public ResponseEntity<Booking> 
+    public ResponseEntity<Booking> addBooking(@RequestBody Booking booking){
+        bookingService.createBooking(booking);
+        return new ResponseEntity<>(booking,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/bookings/{id}")
+    public ResponseEntity<Booking> getBookById(@PathVariable Long id){
+        Booking booking = bookingService.getBookingById(id);
+        if(booking != null){
+            return new ResponseEntity<>(booking,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/api/bookings")
+    public ResponseEntity<List<Booking>> getAllBooking(){
+        List<Booking> blist = bookingService.getAllBookings();
+        if(blist.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(blist,HttpStatus.OK);
+    }
+
+    @PutMapping("/api/bookings/{id}")
+    public ResponseEntity<Booking> modifyBooking(@PathVariable Long id, @RequestBody Booking booking){
+        Booking b = bookingService.updateBooking(id, booking);
+        if(b!= null){
+            return new ResponseEntity<>(b,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
     
 }
