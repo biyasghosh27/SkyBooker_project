196e2087f26403b4f3470f6650df41657b74b771
neojarvis <techadmin@iamneo.ai>
2025-06-15 06:09:37 +0000

    Submit-project-save-commit-6/15/2025--11:39:29-AM

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -33,7 +33,7 @@ public class User {
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) 
+    public void setUsername(String username) {
         this.username = username;
     }
     public String getMobileNumber() {
