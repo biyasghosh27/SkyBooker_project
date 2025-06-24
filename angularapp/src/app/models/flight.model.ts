19b68bc2b2305d2d6fa8c209b8ce541fe68149e3
neojarvis <techadmin@iamneo.ai>
2025-06-24 12:06:17 +0000

    Submit-project-save-commit-6/24/2025--5:36:12-PM

---
+++ angularapp/src/app/models/flight.model.ts
@@ -0,0 +1,10 @@
+interface Flight{
+    flightId:number;
+    flightNumber:string;
+    airline:string;
+    departureLocation:string;
+    arrivalLocation:string;
+    departureTime:string;//change to string for now
+    arrivalTime:string;//change to string for now
+    price:number;
+}
---
+++ angularapp/src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+interface User{
+    userId:number;
+    email:string;
+    password:string;
+    username:string;
+    mobileNumber:string;
+    userRole:string;
+}
