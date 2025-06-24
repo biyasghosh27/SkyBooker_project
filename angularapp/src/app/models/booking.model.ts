717f42759f639c4ab101d2f24bce2a7c0fff7537
neojarvis <techadmin@iamneo.ai>
2025-06-24 12:10:23 +0000

    Auto-save-commit-6/24/2025--5:40:19-PM

--- angularapp/src/app/models/booking.model.ts
+++ angularapp/src/app/models/booking.model.ts
@@ -1,8 +1,8 @@
 interface Booking{
-    bookingId:number;
-    flightId:number;//reference to flight model
-    userId:number;//reference to user model
-    bookingDate:Date;
-    numberOfPassengers:number;
-    status:string//CONFIRMED,CANCELED
+    bookingId?:number;
+    flightId?:number;//reference to flight model
+    userId?:number;//reference to user model
+    bookingDate?:Date;
+    numberOfPassengers?:number;
+    status?:string//CONFIRMED,CANCELED
 }
