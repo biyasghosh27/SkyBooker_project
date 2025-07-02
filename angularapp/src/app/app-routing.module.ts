e1d1b34c77bb02750e7a5d4ffe4b1229f4f1e6a7
neojarvis <techadmin@iamneo.ai>
2025-07-02 17:02:54 +0000

    Auto-save-commit-7/2/2025--10:32:48-PM

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddFlightComponent } from './components/add-flight/add-flight.component';
+import { FlightListComponent } from './components/flight-list/flight-list.component';
+import { ManageBookingComponent } from './components/manage-booking/manage-booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"add-flight/:id",component:AddFlightComponent},
+  {path:"add-flight",component:AddFlightComponent},
+  {path:"flight-list",component:FlightListComponent},
+  {path:"manage-booking",component:ManageBookingComponent},
+  {}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
