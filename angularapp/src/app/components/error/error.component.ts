ddacea002f4aedd60ed3381166e96fa72cc57581
neojarvis <techadmin@iamneo.ai>
2025-07-03 12:27:19 +0000

    Manual-save-commit-7/3/2025--5:57:15-PM

--- angularapp/src/app/components/error/error.component.ts
+++ angularapp/src/app/components/error/error.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  templateUrl: `<h2>404 - Page not found</h2>`
 })
 export class ErrorComponent implements OnInit {
 
