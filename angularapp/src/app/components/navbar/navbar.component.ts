957ed485b76fbc39a37852d105002e35cd3410b1
neojarvis <techadmin@iamneo.ai>
2025-07-03 17:41:55 +0000

    Manual-save-commit-7/3/2025--11:11:40-PM

--- angularapp/src/app/components/navbar/navbar.component.ts
+++ angularapp/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  showDropdown = false;
+  showLogoutPopup = false;
+  isLoggedIn = false;
+  userRole = '';
+  userName = '';
+
+  constructor(private router:Router, private authService:AuthService) { }
 
   ngOnInit(): void {
+    
   }
 
 }
