5382781d801373984c3be6d5b9055108cb7dcb37
neojarvis <techadmin@iamneo.ai>
2025-07-06 20:01:02 +0000

    Manual-save-commit-7/7/2025--1:30:54-AM

--- angularapp/src/app/components/my-history/my-history.component.ts
+++ angularapp/src/app/components/my-history/my-history.component.ts
@@ -12,7 +12,9 @@ export class MyHistoryComponent implements OnInit {
   bookings:any[] = [];
   errorMessage:string = '';
   userId:number = Number(localStorage.getItem('userId'));
-  userEmail:string = String(localStorage.getItem('userEmail'));
+  userEmail:string = String(localStorage.getItem('userEmail'))||'';
+  showModal:boolean = false;
+  modalMessage:string = '';
   
   constructor(private bookingService:BookingService,
     private authService:AuthService) { }
@@ -27,9 +29,24 @@ export class MyHistoryComponent implements OnInit {
     const userId = this.authService.getUserId();
 
     this.bookingService.getBookingsByUserId(userId).subscribe({
-      next:(data)=>this.bookings = data,
+      next:(data)=>{
+        this.bookings = data,
+        this.checkForStatusChange();
+      },
       error:()=>this.errorMessage = 'Failed to load your bookings.'
     });
+  }
+
+  checkForStatusChange():void{
+    const approvedBooking = this.bookings.find(b => b.status === 'Approved');
+    const rejectedBooking = this.bookings.find(b => b.status === 'Rejected');
+    if(approvedBooking){
+      this.modalMessage = `Booking Approved for ${this.userEmail}`;
+      this.showModal = true;
+    }else if(rejectedBooking){
+      this.modalMessage = `Booking Rejected for ${this.userEmail}`;
+      this.showModal = true;
+    }
   }
 
 }
